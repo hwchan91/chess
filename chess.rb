2b12a0bc92f7260803a5bfd371b8261fc9da9942
Dion Chan <hwchan91@outlook.com>
2017-05-06 13:16:12 +0800

    for transfer

--- chess.rb
+++ chess.rb
@@ -16,11 +16,14 @@ black = Player.new("Black", [" K", " Q", " R", " B", " N", " P"], 1)
 
 
 class Board
-  attr_accessor :board
+  attr_accessor :board, :adv2
 
   def initialize
     pieces_symbol
     @board = empty_board
+    @adv2 = []
+    @white = Player.new("White", [" k", " q", " r", " b", " n", " p"], -1)
+    @black = Player.new("Black", [" K", " Q", " R", " B", " N", " P"], 1)
     new_game
     print_board
   end
@@ -177,6 +180,7 @@ class Board
     displace_by(displacements, player, pos)
   end
 
+
   def displace_by(displacements, player, pos)
     valid_moves = []
     row = pos[0] ; col = pos[1]
@@ -207,35 +211,35 @@ class Board
           end
         end
       end
-      #advance 2 sqaures as first move to empty space
-      if player.color == "White" and row = 6 #i.e. rank 2
-        valid_moves << [row - 2, col] if @board[row - 2][col] == @@nil
-      elsif player.color == "Black" and row = 1 #i.e. rank 7
-        valid_moves << [row + 2, col] if @board[row + 2][col] == @@nil
-      end
     end
+    valid_moves = valid_moves + pawn_adv2(player, pos) + en_passant(player, pos)
+
   end
 
-  def check_move(player, start, finish) #[row,col],[row,col]
-    piece = piece_in_square(start)
-    if player.moves.include? piece
-      case piece
-      when @@b_pawn, @@w_pawn
-        pawn_moves(player, start).include? finish
-      when @@b_knight, @@w_knight
-        knight_moves(player, start).include? finish
-      when @@b_rook, @@w_rook
-        rook_moves(player, start).include? finish
-      when @@b_bishop, @@w_bishop
-        bishop_moves(player, start).include? finish
-      when @@b_queen, @@w_queen
-        queen_moves(player, start).include? finish
-      when @@b_king, @@w_king
-        king_moves(player, start).include? finish
-      end
-    else
-      false
+  def pawn_adv2(player, pos)
+    valid_moves = []
+    row = pos[0] ; col = pos[1]
+    if player.color == "White" and row == 6 #i.e. rank 2
+      valid_moves << [row - 2, col] if @board[row - 2][col] == @@nil
+    elsif player.color == "Black" and row == 1 #i.e. rank 7
+      valid_moves << [row + 2, col] if @board[row + 2][col] == @@nil
     end
+    valid_moves
+  end
+
+  #def pawn_adv2?(player, pos)
+  #  pawn_adv2(player, pos).size != 0
+  #end
+
+  def en_passant(player, pos)
+    valid_moves = []
+    row = pos[0] ; col = pos[1]
+    if [row, col + 1] == @adv2
+      valid_moves << [row + player.pawn_dir, col + 1]
+    elsif [row, col - 1] == @adv2
+      valid_moves << [row + player.pawn_dir, col - 1]
+    end
+    valid_moves
   end
 
   def piece_in_square(pos)
@@ -243,7 +247,102 @@ class Board
     @board[row][col]
   end
 
-  
+  def check_move(player, start, finish) #[row,col],[row,col] BUT only if the move doesnt cause the players own king in check - TBC
+    valid = false
+    piece = piece_in_square(start)
+    if player.moves.include? piece
+      valid_moves = find_valid_moves(piece, player, start)
+      if valid_moves.include? finish
+        puts "a"
+        #the following tests if the move puts the player in check
+        temp_board = @board.clone #saves a copy of @board & @adv2
+        temp_adv2 = @adv2.clone
+        8.times { |i| temp_board[i] = @board[i].clone } #Note: it seems each row of temp_board is linked with @board
+
+        make_move(player, start, finish)
+        valid = true if !check?(oppo_player(player))
+        @board = temp_board
+        @adv2 = temp_adv2
+      end
+    end
+    valid
+  end
+
+
+  def oppo_player(player)
+    player.color == "White" ? @black : @white
+  end
+
+  def make_move(player, start, finish)
+      piece = piece_in_square(start)
+      #if the piece is a pawn
+      if [@@b_pawn, @@w_pawn].include? piece
+        @board[@adv2[0]][@adv2[1]] = @@nil if finish == en_passant(player, start)[0] #remove the opponent pawn if en passant
+        @adv2 = finish == pawn_adv2(player, start)[0] ? finish : [] #if pawn advances 2 steps, set @pawn_adv2 to the finish square; otherwise, reset @pawn_adv2
+      else
+        @adv2 = []
+      end
+      @board[finish[0]][finish[1]] = piece
+      @board[start[0]][start[1]] = @@nil
+  end
+
+  def valid_make(player, start, finish)
+    make_move(player, start, finish) if check_move(player, start, finish)
+    print_board
+  end
+
+  def all_valid_moves(player)
+    valid_moves = []
+    player.moves.each do |piece|
+      sqaures_with_piece = find_piece(piece)
+      sqaures_with_piece.each do |pos|
+        valid_moves += find_valid_moves(piece, player, pos)
+      end
+    end
+    valid_moves
+  end
+
+  def find_valid_moves(piece, player, pos)
+    case piece
+    when @@b_pawn, @@w_pawn
+      pawn_moves(player, pos)
+    when @@b_knight, @@w_knight
+      knight_moves(player, pos)
+    when @@b_rook, @@w_rook
+      rook_moves(player, pos)
+    when @@b_bishop, @@w_bishop
+      bishop_moves(player, pos)
+    when @@b_queen, @@w_queen
+      queen_moves(player, pos)
+    when @@b_king, @@w_king
+      king_moves(player, pos)
+    end
+  end
+
+  def check?(player)
+    check = false
+    if player.color == "White"
+      all_valid_moves(player).each do |pos|
+        row = pos[0]; col = pos[1]
+        check = true if @board[row][col] == @@b_king
+      end
+    elsif player.color == "Black"
+      all_valid_moves(player).each do |pos|
+        row = pos[0]; col = pos[1]
+        check = true if @board[row][col] == @@w_king
+      end
+    end
+    check
+  end
+
+
 end
 
-board = Board.new
+#board = Board.new
+#board.board[1][4] = "  "
+#board.board[2][4] = " R"
+#board.board[3][4] = " r"
+board.print_board
+#board.make_move(black, [2,4], [2,3])
+#board.print_board
+board.valid_make(black, [2,3], [2,4])
